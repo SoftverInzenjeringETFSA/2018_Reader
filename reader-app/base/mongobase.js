42d3c3101f7ecf99e4c4775f574019b41899d6ec
Your Name <you@example.com>
2018-05-16 00:12:01 +0200

    Dodan dio koji je vezan za mobilnu aplikaciju

--- reader-app/base/mongobase.js
+++ reader-app/base/mongobase.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://127.0.0.1/reader');
-mongoose.connect('mongodb://loacalhost/reader'); //dodao zbog testiranja
+mongoose.connect('mongodb://127.0.0.1/reader');
+//mongoose.connect('mongodb://loacalhost/reader'); //dodao zbog testiranja
 
 module.exports = mongoose;
